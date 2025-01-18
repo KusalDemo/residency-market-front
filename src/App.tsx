29419af0203732edc9da95eb9f774ad4a924be29
KusalDemo <kusalgunasekara2002@gmail.com>
2025-01-19 03:19:35 +0530

    Fixed issue could not find react-redux context value. Wrapped the component from  <Provider>

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {Home} from "./pages/Home.tsx";
 import {Footer} from "./components/Footer.tsx";
 import {Contact} from "./pages/Contact.tsx";
 import {About} from "./pages/About.tsx";
 import {Header} from "./components/Header.tsx";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 
 function App() {
     return (
-        <Router>
-            <div className="min-h-screen bg-gray-50 flex flex-col">
-                <Header/>
-                <main className="flex-grow">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div className="min-h-screen bg-gray-50 flex flex-col">
+                    <Header/>
+                    <main className="flex-grow">
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/about" element={<About/>}/>
+                            <Route path="/contact" element={<Contact/>}/>
+                        </Routes>
+                    </main>
+                    <Footer/>
+                </div>
+            </Router>
+        </Provider>
     )
 }
 
